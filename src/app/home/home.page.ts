b4ea286777127762898edd3d0c08e3d22eec5ecd
AshesToAshes13 <51061106+AshesToAshes13@users.noreply.github.com>
2023-04-16 17:38:43 +0300

    Подключил API получения списка пива

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import {BeerService} from "../services/beer.service";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,14 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor(private beerService: BeerService) {}
+
+  beersList = []
+  ngOnInit(): void {
+    this.beerService.getAllBeers().subscribe((beers) => {
+      console.log(beers)
+      this.beersList = beers as []
+    })
+  }
 }
---
+++ src/app/services/beer.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BeerService {
+  constructor(private http: HttpClient) {
+  }
+  getAllBeers() {
+    return this.http.get('https://api.punkapi.com/v2/beers')
+  }
+}
