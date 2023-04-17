b4cc35915ea5ce9be71d1838dd50fae0b193ec7d
AshesToAshes13 <51061106+AshesToAshes13@users.noreply.github.com>
2023-04-18 01:05:51 +0300

    Внешний вид кода

--- src/app/components/beer/beer.component.ts
+++ src/app/components/beer/beer.component.ts
@@ -1,19 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {BeerInterface} from "../../interfaces/beer.interface";
 import {IonicModule} from "@ionic/angular";
-import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-beer',
   templateUrl: './beer.component.html',
   styleUrls: ['./beer.component.scss'],
   standalone: true,
-  imports: [IonicModule, RouterLink]
+  imports: [IonicModule]
 })
 export class BeerComponent {
 
   constructor() {}
 
   @Input() beer: BeerInterface
-
 }
--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {BeerInterface} from "../interfaces/beer.interface";
-import {delay} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,8 @@ export class BeerService {
   constructor(private http: HttpClient) {
   }
   getAllBeers(page: number) {
-    return this.http.get<BeerInterface[]>('https://api.punkapi.com/v2/beers', {
+    return this.http.
+      get<BeerInterface[]>('https://api.punkapi.com/v2/beers', {
       params: new HttpParams({
         fromObject: {page: page, per_page: 5}
       })
@@ -19,6 +19,7 @@ export class BeerService {
   }
 
   getSingleBeer(id: number) {
-    return this.http.get<BeerInterface[]>(`https://api.punkapi.com/v2/beers/${id}`)
+    return this.http.
+      get<BeerInterface[]>(`https://api.punkapi.com/v2/beers/${id}`)
   }
 }
